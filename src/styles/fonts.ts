a66b785721d38682519e02cccb65711c345556e5
Andrey <andreyevyk13@gmail.com>
2022-01-19 20:32:17 -0300

    feat: add font-face differents weights

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,12 +1,22 @@
 import { css } from 'styled-components'
 import HelveticaNeueRegular from 'assets/fonts/HelveticaNeue-Regular.otf'
+import HelveticaNeueBold from 'assets/fonts/HelveticaNeue-Bold.otf'
+import HelveticaNeueMedium from 'assets/fonts/HelveticaNeue-Medium.otf'
 
 export const customFonts = css`
   @font-face {
     font-family: 'Helvetica Neue';
     font-weight: normal;
-    font-style: normal;
-    font-display: block;
     src: ${HelveticaNeueRegular};
+  }
+  @font-face {
+    font-family: 'Helvetica Neue';
+    font-weight: 500;
+    src: ${HelveticaNeueMedium};
+  }
+  @font-face {
+    font-family: 'Helvetica Neue';
+    font-weight: 700;
+    src: ${HelveticaNeueBold};
   }
 `
