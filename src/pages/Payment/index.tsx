5a8622f9440fc2901763cb785c5e89aeef300bc0
Andrey <andreyevyk13@gmail.com>
2022-01-25 14:52:16 -0300

    fix: remove unused variables

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from 'atoms/Button';
 import { PaymentForm } from 'organisms/PaymentForm';
 import { PriceSummary } from 'organisms/PriceSummary';
-import { FieldValues, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { ICart } from 'pages/Cart';
